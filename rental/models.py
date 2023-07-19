1819814dac531afbc748d79c2ef2e137b1b99d8d
ourfalia <>
2023-07-19 10:15:49 +0000

    add book and reservation to rental models and register them

--- rental/models.py
+++ rental/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='book_images/', blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+class Reservation(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    is_checked_out = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.book.title} - {self.start_date} to {self.end_date}"
