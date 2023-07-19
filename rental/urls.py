8c51be1412dc2d1a1c19e9f871b7f985104b44fd
ourfalia <>
2023-07-19 17:02:07 +0000

    add book detail views, urls, and template

--- rental/urls.py
+++ rental/urls.py
@@ -6,4 +6,5 @@ from . import views
 
 urlpatterns = [
     path('book_list', views.book_list, name='book_list'),
+    path('book/<int:pk>/', views.book_detail, name='book_detail'),
 ]
--- rental/views.py
+++ rental/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Book
 from django.contrib.auth.decorators import login_required
 
@@ -10,3 +10,8 @@ def book_list(request):
     books = Book.objects.all()
     return render(request, 'rental/book_list.html', {'books': books})
     
+
+@login_required
+def book_detail(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    return render(request, 'rental/book_detail.html', {'book': book})
